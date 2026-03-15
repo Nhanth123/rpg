34cbe50cad0b9a5889d3106ecf9bc2303f5b8968
nhan.trinh <hongnhan50@gmail.com>
2026-03-15 16:41:37 +0700

    feature: PlayerHurt's animation

--- Gameplay/Scene/PlayerHurt.gd
+++ Gameplay/Scene/PlayerHurt.gd
@@ -3,9 +3,12 @@ extends StateBase
 var pushBackDir : Vector3
 var pushBackSpeed: float  = 50
 
+@export var materialEffectAnimationPlayer: AnimationPlayer
+
 func enter():
 	super.enter()
 	character.velocity  = Vector3.ZERO
+	materialEffectAnimationPlayer.play('Hurt_flash')
 
 func state_update(_delta: float):
 	character.velocity = pushBackDir * pushBackSpeed * _delta
