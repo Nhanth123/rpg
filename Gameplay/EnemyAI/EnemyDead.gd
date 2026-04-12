8465cd2c5078c2aad71061eb04da71ae59c59e55
nhan.trinh <hongnhan50@gmail.com>
2026-04-12 22:46:07 +0700

    feature: Enemy drop items

--- Gameplay/EnemyAI/EnemyDead.gd
+++ Gameplay/EnemyAI/EnemyDead.gd
@@ -15,5 +15,13 @@ func enter():
 	materialEffectAnimationPlayer.play('Dead')
 	
 	await get_tree().create_timer(3).timeout
+	
+	var dropitem= preload("res://Asset/VFX/Scene/collectable_healing_orb.tscn")
+	
+	var dropitemInstance = dropitem.instantiate()
+	get_tree().get_root().get_node("Node3D").add_child(dropitemInstance)
+	dropitemInstance.global_position = character.global_position
+	
+	
 	character.queue_free()
 	
--- Gameplay/Scene/Collectable_HealingOrb.gd
+++ Gameplay/Scene/Collectable_HealingOrb.gd
@@ -3,13 +3,16 @@ extends Node3D
 var healthValue: int  = 30
 
 @onready var Visual: Node3D = $VisualNode
+@onready var VFXanimationPlayer: AnimationPlayer = $VFX/AnimationPlayer
 
 
-func _process(delta):
-	pass
+func _process(_delta):
+	if Visual.visible == false && VFXanimationPlayer.is_playing() == false:
+		queue_free()
 
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Player"):
 		body.AddHealth(healthValue)
 		Visual.visible = false
+		VFXanimationPlayer.play("PlayParticle")
