42a19f5f7d27bec397f559f8d8a880f82fc71ba2
nhan.trinh <hongnhan50@gmail.com>
2026-05-16 20:32:46 +0700

    feature: setup  button at game level

---
+++ Gameplay/Scene/GameLevel.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_btn_restart_button_up():
+	print('btn restart clicked')
+
+
+func _on_btn_main_menu_button_up():
+	print('btn main clicked')
+
+
+func _on_bth_resume_button_up():
+	print('btn resume clicked')
